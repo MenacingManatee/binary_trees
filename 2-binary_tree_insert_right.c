889e4096753ada400e5c15ac942158e3bda9ef69
Brett Davis <brett.p.davis101@gmail.com>
2020-07-21 00:30:36 +0000

    Same as inserting to the left, but swaps left for right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,12 +1,27 @@
+#include "binary_trees.h"
 #include <stdio.h>
 
 /**
- * main - 
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * @parent: pointer to the node to insert the right-child in
+ * @value: value to store in the new node
  *
- * Return: Always 0 (ok)
+ * Return: pointer to created node, or NULL on failure
  */
-int main(void)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	
-	return (0);
+	binary_tree_t *new, *right;
+
+	if (parent == NULL)
+		return (NULL);
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+	right = parent->right;
+	new = binary_tree_node(parent, value);
+	new->right = right;
+	if (right != NULL)
+		right->parent = new;
+	parent->right = new;
+	return (new);
 }

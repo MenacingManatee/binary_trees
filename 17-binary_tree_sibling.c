ee145c17fb87c84e404527ba3802607b1384e38d
Brett Davis <brett.p.davis101@gmail.com>
2020-07-22 06:33:12 +0000

    Checks for node being null, then the parent, then returns the sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
+#include "binary_trees.h"
 
 /**
- * main - 
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: node to find the sibling of
  *
- * Return: Always 0 (ok)
+ * Return: Sibling of the node
  */
-int main(void)
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	
-	return (0);
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
 }

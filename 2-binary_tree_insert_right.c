884ee2456cbdae930ea1638b7f8535fc6d5ad411
Brett Davis <brett.p.davis101@gmail.com>
2020-07-22 23:55:40 +0000

    Adds extra NULL check before dereferencing new

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -16,6 +16,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 	right = parent->right;
 	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
 	new->right = right;
 	if (right != NULL)
 		right->parent = new;

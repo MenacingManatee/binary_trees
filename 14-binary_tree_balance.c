f40d0288b8c101a8c1677760b714c105ae810c97
Brett Davis <brett.p.davis101@gmail.com>
2020-07-22 02:40:14 +0000

    Calls the binary tree height function on both sides, then returns the difference

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,12 +1,39 @@
+#include "binary_trees.h"
 #include <stdio.h>
 
 /**
- * main - 
+ * height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height.
  *
- * Return: Always 0 (ok)
+ * Return: Height of the tree, or 0 on failure
  */
-int main(void)
+size_t height(const binary_tree_t *tree)
 {
-	
-	return (0);
+	size_t count1, count2;
+
+	if (tree == NULL)
+		return (-1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	count1 = height(tree->left);
+	count2 = height(tree->right);
+	if (count1 > count2)
+		return (count1 + 1);
+	return (count2 + 1);
+}
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ *
+ * Return: balance of the tree
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (tree == NULL)
+		return (0);
+	left = height(tree->left);
+	right = height(tree->right);
+	return (left - right);
 }

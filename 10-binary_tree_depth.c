7de4a9d5d7178a619446900f6fe83a6d702cd0f6
Brett Davis <brett.p.davis101@gmail.com>
2020-07-22 00:44:25 +0000

    Uses recursion to find the depth of any given node

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,12 +1,17 @@
-#include <stdio.h>
+#include "binary_trees.h"
 
 /**
- * main - 
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree: pointer to the node to measure the depth
  *
- * Return: Always 0 (ok)
+ * Return: depth of the node, or 0 on failure
  */
-int main(void)
+size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	
-	return (0);
+	size_t count;
+
+	if (tree == NULL || tree->parent == NULL)
+		return (0);
+	count = (1 + binary_tree_depth(tree->parent));
+	return (count);
 }

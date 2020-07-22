b446ec94d9cf862fca9cb5e5fa915406d8297a66
Brett Davis <brett.p.davis101@gmail.com>
2020-07-22 06:24:45 +0000

    Counts the number of nodes with only 1 child. If the number is 0, the tree is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,12 +1,33 @@
-#include <stdio.h>
+#include "binary_trees.h"
 
 /**
- * main - 
+ * half_nodes - counts the nodes with only one child
+ * @tree: pointer to the root node of the tree to measure the size
  *
- * Return: Always 0 (ok)
+ * Return: the size of a binary tree, or 0 on failure
  */
-int main(void)
+size_t half_nodes(const binary_tree_t *tree)
 {
-	
-	return (0);
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	if ((tree->left && !tree->right) || (!tree->left && tree->right))
+		return (1);
+	return ((half_nodes(tree->left)) + (
+			half_nodes(tree->right)));
+}
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 0 if not full, 1 if full
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (half_nodes(tree) != 0)
+		return (0);
+	return (1);
 }

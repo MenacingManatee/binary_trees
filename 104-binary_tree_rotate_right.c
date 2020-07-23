2fc22fc2506b4a405ffcc4ff0fc9af7986745bdb
Brett Davis <brett.p.davis101@gmail.com>
2020-07-23 02:51:16 +0000

    Same as 103 but rotates right instead of left

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -1,12 +1,23 @@
-#include <stdio.h>
+#include "binary_trees.h"
 
 /**
- * main - 
+ * binary_tree_rotate_right - performs a left-rotation on a binary tree
+ * @tree: pointer to the root node of the tree to rotate
  *
- * Return: Always 0 (ok)
+ * Return: pointer to the new root node of the tree once rotated
  */
-int main(void)
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-	
-	return (0);
+	binary_tree_t *right;
+
+	if (tree == NULL || tree->left == NULL)
+		return (NULL);
+	right = tree->left->right;
+	tree->left->parent = NULL;
+	tree->parent = tree->left;
+	tree->left = right;
+	tree->parent->right = tree;
+	if (right != NULL)
+		right->parent = tree;
+	return (tree->parent);
 }

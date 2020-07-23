fb908ec4ee7ac90472484ac8f2f532af6d7118c3
Brett Davis <brett.p.davis101@gmail.com>
2020-07-23 00:35:57 +0000

    I had to cast the return because it complained I was dropping the const qualifier

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,12 +1,22 @@
-#include <stdio.h>
+#include "binary_trees.h"
 
 /**
- * main - 
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * @first: pointer to the first node
+ * @second: pointer to the second node
  *
- * Return: Always 0 (ok)
+ * Return: pointer to the lowest common ancestor node of the two given nodes
  */
-int main(void)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
 {
-	
-	return (0);
+	const binary_tree_t *tmp, *tmp2;
+
+	for (tmp = first; tmp != NULL; tmp = tmp->parent)
+		for (tmp2 = second; tmp2 != NULL; tmp2 = tmp2->parent)
+		{
+			if (tmp == tmp2)
+				return ((binary_tree_t *)tmp);
+		}
+	return (NULL);
 }

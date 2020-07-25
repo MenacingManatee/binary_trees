75929427a9a6612411b0eb6a330a9ffe1cf4d06d
Brett Davis <brett.p.davis101@gmail.com>
2020-07-25 00:26:17 +0000

    I really overcomplicated this, 111 will already not insert if there's a duplicate value, no need to clean the array

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
+#include "binary_trees.h"
 
 /**
- * main - 
+ * array_to_bst - builds a Binary Search Tree from an array
+ * @array: pointer to the first element of the array to be converted
+ * @size: the number of element in the array
  *
- * Return: Always 0 (ok)
+ * Return: a pointer to the root node of the created BST, or NULL on failure
  */
-int main(void)
+bst_t *array_to_bst(int *array, size_t size)
 {
-	
-	return (0);
+	bst_t *root;
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return (NULL);
+	root = NULL;
+	for (i = 0; i < size; i++)
+		bst_insert(&root, array[i]);
+	return (root);
 }
